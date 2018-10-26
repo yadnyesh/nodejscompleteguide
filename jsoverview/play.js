e52a2b9df6a3764a5b2791492fe8df2c1227f7c7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-26 19:12:34 +0530

    efficient syntax

--- jsoverview/play.js
+++ jsoverview/play.js
@@ -12,4 +12,4 @@ const hobbies = ['Sports', 'Cooking'];
 for (let hobby of hobbies) {
   console.log(hobby);
 }
-console.log(hobbies.map(hobby => hobby));
+console.log(hobbies.map(hobby =>'Hobby: ' + hobby));
