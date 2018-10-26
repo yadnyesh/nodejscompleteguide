9a6ded68726c15b70a7ddc0d834955c8797012cc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-26 10:32:28 +0530

    First JS code

---
+++ jsoverview/play.js
@@ -0,0 +1,9 @@
+var name = 'Max';
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return('Name is: ' + userName + ', age is: '+ userAge + ', has hobbies? ' + userHasHobby);
+}
+
+console.log(summarizeUser('Yadnyesh', 37, true));
