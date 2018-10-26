321437c74d751e8895e905ba552f3b53e98be8f7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-26 18:16:12 +0530

    Another function

--- jsoverview/play.js
+++ jsoverview/play.js
@@ -8,4 +8,10 @@ const summarizeUser =  (userName, userAge, userHasHobby) => {
   return('Name is: ' + userName + ', age is: '+ userAge + ', has hobbies? ' + userHasHobby);
 }
 
+const add = (a, b) => {
+  return a + b;
+}
+
+console.log(add(2,6));
+
 console.log(summarizeUser('Yadnyesh', 37, true));
