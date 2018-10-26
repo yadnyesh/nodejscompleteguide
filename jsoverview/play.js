3e863c5d5582b19218ea303380be1bb21f1e7a02
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-26 20:09:09 +0530

    REST and SPREAD operator are same

--- jsoverview/play.js
+++ jsoverview/play.js
@@ -16,3 +16,9 @@ console.log(hobbies.map(hobby =>'Hobby: ' + hobby));
 
 const copiedArray = [...hobbies];
 console.log(copiedArray);
+
+const toArray = (...args) => {
+  return args;
+}
+
+console.log(toArray(1,2,3,4));
