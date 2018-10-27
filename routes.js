8a407ca8091b15043d97703a384cf45c5786216d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-27 19:33:07 +0530

    after installing nodemon

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ const requestHandler = (req, res) => {
 
   if(url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
+    res.write('<head><title>Enter Message Now</title></head>');
     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form><body>');
     res.write('</html>');
     return res.end();
