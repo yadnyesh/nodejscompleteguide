339daf3e1a99bc4869e37aaca6047775b717c430
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-27 11:25:40 +0530

    filter request based on path

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
+
+  const url = req.url;
+  if(url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" methog="POST"><input type="text" name="message"><button type="submit">Submit</button></form><body>');
+    res.write('</html>');
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title></head>');
