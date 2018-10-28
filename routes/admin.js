f663b270359db61164a33537a8df3deb19b4bb5d
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 08:56:38 +0530

    fix typo

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
+router.get('/add-product', (req, res, next) => {
   res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form><body></html>');
 });
 
