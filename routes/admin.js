6c5d573b694154d99580dcfe2eaeb05c3300de30
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 16:25:17 +0530

    Use rootDir

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+const rootDir = require('../util/path');
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next ) =>{
