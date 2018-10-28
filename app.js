cd5931f4b0741ed71f75149e4bb28ab96e84193e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 08:58:36 +0530

    add Admin routes to app.js

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 const http = require('http');
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(adminRoutes);
 
-app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware');
-  res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form><body></html>');
-});
 
-app.post('/product', (req, res, next ) =>{
-  console.log(req.body);
-  res.redirect('/');
-});
 
 app.use('/', (req, res, next) => {
   console.log('In root middleware');
