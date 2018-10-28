66c5bbb4c5fe457b3e8409c739aadea23a48ef05
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 10:06:02 +0530

    refactor: admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
 
 router.get('/add-product', (req, res, next) => {
-  res.send('<html><body><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form><body></html>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next ) =>{
