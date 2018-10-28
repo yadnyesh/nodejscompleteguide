6d44cb8c22c1b7e16f71ff53c09766c58db332d8
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 09:04:07 +0530

    Added

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
-
-
-
+const shopRoutes = require('./routes/shop');
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
+app.use(shopRoutes);
 
-
-
-app.use('/', (req, res, next) => {
-  console.log('In root middleware');
-  res.send('<html><h1>Hello From Express</h1></html>');
-});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
-app.use('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   console.log('In root middleware');
   res.send('<html><h1>Hello From Express</h1></html>');
 });
+
+module.exports = router
