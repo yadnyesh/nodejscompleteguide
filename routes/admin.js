c0d8ea5a6b37c11d773a4acc1d2f46b2d7869603
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 08:54:37 +0530

    code refactoring - different routes

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.use('/add-product', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form><body></html>');
+});
+
+router.post('/product', (req, res, next ) =>{
+  console.log(req.body);
+  res.redirect('/');
+});
