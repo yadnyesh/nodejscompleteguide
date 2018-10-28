01856fe4f68eb704c4acb2942c59a3c960391b97
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 08:39:41 +0530

    Fix body-parser deprecated issue

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded);
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
   console.log('In another middleware');
