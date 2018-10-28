0de1bc9d8df69eccef9398e6c5c431dbbf8d53f7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 09:12:16 +0530

    refactoring

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form><body></html>');
+  res.send('<html><body><form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form><body></html>');
 });
 
-router.post('/product', (req, res, next ) =>{
+router.post('/add-product', (req, res, next ) =>{
   console.log(req.body);
   res.redirect('/');
 });
