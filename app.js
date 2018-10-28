f1f289e3c6409ddbafae584729ddde7daaa0ee61
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 08:36:08 +0530

    Added body Parser

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const http = require('http');
-const routes = require('./routes');
+
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded);
 
-app.use('/product', (req, res, next) => {
+app.use('/add-product', (req, res, next) => {
   console.log('In another middleware');
-  res.send('<html><h1>Hello From Product express</h1></html>');
+  res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form><body></html>');
+});
+
+app.use('/product', (req, res, next ) =>{
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In another middleware');
+  console.log('In root middleware');
   res.send('<html><h1>Hello From Express</h1></html>');
 });
 
