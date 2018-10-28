1877623f83f861c7baf417f2f5a813042b73d509
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 16:21:42 +0530

    Display 404 successfully for invalid routes

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const path = require('path');
 const app = express();
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
