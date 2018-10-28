ac5b60a5c7b9fba1eb30972db57ebf3413a44aae
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 08:26:54 +0530

    multiple path routing with Express

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require('express');
 
 const app = express();
 
+app.use('/product', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<html><h1>Hello From Product express</h1></html>');
+});
+
 app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<html><h1>Hello From Express</h1></html>');
