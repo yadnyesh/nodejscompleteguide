d746f101f2dd19263d73a2779ed343bcd6af23d1
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-10-28 18:51:42 +0530

    79: Views path

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'pug');
+app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
