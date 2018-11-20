8aa5953333eaf8bd1832add8475aadea6d096a45
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-20 13:14:09 +0530

    highlight active tabs

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
   console.log(adminData.products);
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'Shop'});
+  res.render('shop', {prods: products, docTitle: 'Shop', path: '/' });
 });
 
 module.exports = router
