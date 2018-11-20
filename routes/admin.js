4d10373706127eceb3ea15859a0709b6d5beb894
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-20 10:10:12 +0530

    app working except images

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const rootDir = require('../util/path');
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {pageTitle: 'Add Product'})
 });
 
 router.post('/add-product', (req, res, next ) =>{
