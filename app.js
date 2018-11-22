fab922b9d16a703d4b246acb3faa09eaa525382a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-11-22 10:19:39 +0530

    Config changes for HBS Layout

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoutes = require('./routes/shop');
 const path = require('path');
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
 app.set('view engine', 'hbs');
 app.set('views', 'views')
 
